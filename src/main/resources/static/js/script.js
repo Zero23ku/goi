e7518753c6f66ac05a98becafe8043cc9b0aa605
Zero <marcelo.cardenas@usach.cl>
2019-06-22 02:14:35 -0400

    feat: user can ask for a new word after a successful answer

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -6,14 +6,31 @@ $(document).ready(() =>{
     });
 });
 
+$("#again").click((e) =>{
+    e.preventDefault();
+    const $send = $("#send"),
+          $again = $("#again");
+    $.get("/goi",(data)=>{
+        $("#goi").text("Palabra: " + data.goi);
+        $send.show();
+        $again.hide();
+    }).fail(()=>{
+        alert("Error :(");
+    });
+});
+
 $("#send").click((e) => {
     e.preventDefault();
     const $goi = $("#goi"),
           $kaitou = $("#kaitou"),
+          $send = $("#send"),
+          $again = $("#again"),
           url = "goi/" + $goi.text().split(":")[1].replace(" ","") + "/kaitou/" + $kaitou.val();
     if($kaitou.val() != null && $kaitou.val() != ""){
         $.get(url,(data)=>{
+            $send.hide();
             alert(data.meaningResult);
+            $again.show();
         }).fail(()=>{
             alert("Error al obtener la respuesta :(")
         });
